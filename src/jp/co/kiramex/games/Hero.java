a309a4bf3150efa0cbf37ceff3e1118fb3dd26a8
Longdinium <rondobell0303@gmail.com>
2023-11-26 23:39:05 +0900

    Heroの更新

--- src/jp/co/kiramex/games/Hero.java
+++ src/jp/co/kiramex/games/Hero.java
@@ -1,5 +1,20 @@
 package jp.co.kiramex.games;
 
 public class Hero extends Character {
+    private int weapon; // 武器の強さ
+
+    // getter
+    public int getWeapon() {
+        return weapon;
+    }
+
+    // setter
+    public void setWeapon(int weapon) {
+        this.weapon = weapon;
+    }
+
+    // 引数なしのコンストラクタ
+    public Hero() {
+    }
 
 }
