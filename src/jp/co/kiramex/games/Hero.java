3b885c9303aef5ede20a801e09d190e8f1bf58c7
Longdinium <rondobell0303@gmail.com>
2023-12-10 16:28:25 +0900

    Characterクラスで定義したattackメソッドのオーバーライド

--- src/jp/co/kiramex/games/Hero.java
+++ src/jp/co/kiramex/games/Hero.java
@@ -23,4 +23,21 @@ public class Hero extends Character {
         this.weapon = weapon;
     }
 
+    // Characterクラスで定義したattackメソッドのオーバーライド
+
+    @Override
+    public void attack(Character opponent) {
+        // （自分の攻撃力 + 自分の武器の強さ） - 相手の防御力 をダメージ量とする
+        int damage = super.getOffense() + this.getWeapon() - opponent.getDefense();
+
+        // ダメージ量の計算結果がプラスかどうか
+        if (damage > 0) {
+            // ダメージ量がプラスならダメージを与える
+            opponent.setHp(opponent.getHp() - damage);
+            System.out.println(super.getName() + " は " + opponent.getName() + " に " + damage + " のダメージを与えた！");
+        } else {
+            // ダメージ量が0以下ならミスにする
+            System.out.println("ミス！ " + super.getName() + " は " + opponent.getName() + " にダメージを与えられない！");
+        }
+    }
 }
