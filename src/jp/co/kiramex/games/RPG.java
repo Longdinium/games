b59acb93a527cdd678d43da412cd96b4821072ff
Longdinium <rondobell0303@gmail.com>
2023-12-10 15:18:40 +0900

    RPGの更新

--- src/jp/co/kiramex/games/RPG.java
+++ src/jp/co/kiramex/games/RPG.java
@@ -4,8 +4,16 @@ public class RPG {
 
     public static void main(String[] args) {
         // 勇者と敵のキャラクターを生成
+
+        /*
+        オブジェクト指向プログラミング2 Ch2でコメントアウト
+
         Character hero = new Character("勇者", 10, 5, 3);
         Character slime = new Character("スライム", 5, 2, 2);
+         */
+
+        Hero hero = new Hero("勇者", 10, 5, 3, 1);
+        Enemy slime = new Enemy("スライム", 5, 2, 2, "スライム");
 
         hero.attack(slime);     // 勇者 が スライム に攻撃する
         slime.attack(hero);     // スライム が 勇者 に攻撃する
